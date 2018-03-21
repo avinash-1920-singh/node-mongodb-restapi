9bcf35febe8819a4dcdd1fb34a60831b5e6a1683
Avinash singh <avinash.1920.singh@gmail.com>
2018-03-22 00:00:08 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./db/model/todo');
 
 var app=express();
+const port=process.env.PORT||3000;
+
 app.use(bodyParser.json());
 app.post('/todos',(req,res)=>{
     //console.log(req.body);
@@ -48,8 +50,8 @@ app.get('/todos/:id',(req,res)=>{
 
 })
 
-app.listen(3000,()=>{
-    console.log('started on port 3000');
+app.listen(port,()=>{
+    console.log(`started up at port ${port}`);
 })
 
 
